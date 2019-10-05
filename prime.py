cb4894995addc5a1a4204865d18b7cc092d97c4d
Shivani Yadav <yadashiv@sheridancollege.ca>
2019-10-06 01:58:35 +0530

    Done  Step1

--- prime.py
+++ prime.py
@@ -1,3 +1,17 @@
 """
 prime.py -- Write the application code here
 """
+def generate_prime_factors(to_be_evaluated_value):
+    i = 2
+    prime_factor_list = []
+    while i <= to_be_evaluated_value:
+        if (to_be_evaluated_value % i) == 0:
+            prime_factor_list.append(i)
+            to_be_evaluated_value = to_be_evaluated_value / i
+        else:
+            i = i + 1
+    return prime_factor_list
+
+print (generate_prime_factors(92))
+print (generate_prime_factors(9))
+print (generate_prime_factors(10))
--- tests/test_generate_prime_factors.py
+++ tests/test_generate_prime_factors.py
@@ -2,3 +2,10 @@
 """
 The test module for Prime Factors
 """
+import pytest
+from prime import generate_prime_factors
+
+def test_data_type_not_integer():
+    """Checks if the to be evaluated value is not an integer and raise a value error"""
+    if type(generate_prime_factors(to_be_evaluated_value)) != int:
+        pytet.raises(ValueError)

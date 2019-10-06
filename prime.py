a688e52884b4c5f8fef7b46c90df16e8c1119ad9
Shivani Yadav <yadashiv@sheridancollege.ca>
2019-10-07 02:28:06 +0530

    Done Step 2

--- prime.py
+++ prime.py
@@ -3,4 +3,8 @@ prime.py -- Write the application code here
 """
 def generate_prime_factors(number):
     """ Code to generate prime factors """
-    return number
+    prime_factor_list = []
+    if not isinstance(number, int):
+        raise ValueError("Only integers can be used in the function")
+
+    return prime_factor_list
--- tests/test_generate_prime_factors.py
+++ tests/test_generate_prime_factors.py
@@ -9,3 +9,9 @@ def test_data_type_not_integer():
     """Step 1:Checks if the to be evaluated value is not an integer and raise a value error"""
     if not isinstance(generate_prime_factors(2), int):
         raise ValueError("Only integers can be used in the function")
+
+def test_empty_list_on_call_1():
+    """Step 2: if 1 called in function, then list is blank"""
+    prime_factor_list = []
+    if generate_prime_factors(1):
+        print(prime_factor_list)

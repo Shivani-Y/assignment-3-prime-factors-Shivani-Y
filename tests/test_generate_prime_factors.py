dd5f58b3bb8015f010478e91a5d5920f3a6a67bb
Shivani Yadav <yadashiv@sheridancollege.ca>
2019-10-07 01:31:45 +0530

    Step 1 done

--- tests/test_generate_prime_factors.py
+++ tests/test_generate_prime_factors.py
@@ -2,10 +2,10 @@
 """
 The test module for Prime Factors
 """
-import pytest
 from prime import generate_prime_factors
 
 def test_data_type_not_integer():
-    """Checks if the to be evaluated value is not an integer and raise a value error"""
-    if type(generate_prime_factors(to_be_evaluated_value)) != int:
-        pytet.raises(ValueError)
+
+    """Step 1:Checks if the to be evaluated value is not an integer and raise a value error"""
+    if not isinstance(generate_prime_factors(2), int):
+        raise ValueError("Only integers can be used in the function")

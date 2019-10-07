99772865f8bcc406111a329a41c4ec3a76c847ea
Shivani Yadav <yadashiv@sheridancollege.ca>
2019-10-08 02:42:29 +0530

    Redone Step 1: This is the final Step 1

--- prime.py
+++ prime.py
@@ -3,17 +3,6 @@ prime.py -- Write the application code here
 """
 def generate_prime_factors(number):
     """ Code to generate prime factors """
-    prime_factor_list = []
-    i = 2
-    if not isinstance(number, int):
-        ValueError("Only integers can be used in the function")
 
-    while i <= number:
-        if (number%i) == 0:
-            prime_factor_list.append(i)
-            number = number / i
-        else:
-            i = i+1
-    print(prime_factor_list)
-
-    return number
+    if not isinstance(number, int): #raises an error of function called for any type but integer
+        raise ValueError("Only integers can be used in the function")
--- tests/test_generate_prime_factors.py
+++ tests/test_generate_prime_factors.py
@@ -2,94 +2,14 @@
 """
 The test module for Prime Factors
 """
+import pytest
 from prime import generate_prime_factors
+
 
 def test_data_type_not_integer():
 
     """Step 1:Checks if the to be evaluated value is not an integer and raise a value error"""
-    if not isinstance(generate_prime_factors(2.2), int):
-        ValueError("Only integers can be used in the function")
 
-def test_empty_list_on_call_1():
-    """Step 2: if 1 called in function, then list is blank"""
-    prime_factor_list = []
-    if generate_prime_factors(1):
-        print(prime_factor_list)
-
-def test_empty_list_on_call_2():
-    """Step 3: when function called for 2"""
-    i = 2
-    prime_factor_list = []
-    if (generate_prime_factors(2)%i) == 0:
-        prime_factor_list.append(2)
-    print(prime_factor_list)
-
-def test_empty_list_on_call_3():
-    """Step 4: when function called for 3"""
-    i = 2
-    prime_factor_list = []
-    prime = generate_prime_factors(3) #assigned function to a variale
-    while i <= prime:
-        if (prime%i) == 0:
-            prime_factor_list.append(i)
-            prime = prime / i
-        else:
-            i = i+1
-
-    print(prime_factor_list)
-
-def test_empty_list_on_call_4():
-    """Step 5: when function called for 3"""
-    i = 2
-    prime_factor_list = []
-    prime = generate_prime_factors(4) #assigned function to a variale
-    while i <= prime:
-        if (prime%i) == 0:
-            prime_factor_list.append(i)
-            prime = prime / i
-        else:
-            i = i+1
-
-    print(prime_factor_list)
-
-def test_empty_list_on_call_6():
-    """Step 6: when function called for 3"""
-    i = 2
-    prime_factor_list = []
-    prime = generate_prime_factors(6) #assigned function to a variale
-    while i <= prime:
-        if (prime%i) == 0:
-            prime_factor_list.append(i)
-            prime = prime / i
-        else:
-            i = i+1
-
-    print(prime_factor_list)
-
-def test_empty_list_on_call_8():
-    """Step 7: when function called for 3"""
-    i = 2
-    prime_factor_list = []
-    prime = generate_prime_factors(8) #assigned function to a variale
-    while i <= prime:
-        if (prime%i) == 0:
-            prime_factor_list.append(i)
-            prime = prime / i
-        else:
-            i = i+1
-
-    print(prime_factor_list)
-
-def test_empty_list_on_call_9():
-    """Step 8: when function called for 3"""
-    i = 2
-    prime_factor_list = []
-    prime = generate_prime_factors(9) #assigned function to a variale
-    while i <= prime:
-        if (prime%i) == 0:
-            prime_factor_list.append(i)
-            prime = prime / i
-        else:
-            i = i+1
-
-    print(prime_factor_list)
+    with pytest.raises(ValueError):#raises an error of function called for any type but integer
+        if not isinstance(generate_prime_factors(2.2), int):
+            raise ValueError("Function only workS for Integers")

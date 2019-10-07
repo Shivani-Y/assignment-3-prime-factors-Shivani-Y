6f28408f13487b07ff4506b27ca96a0e1dcf6307
Shivani Yadav <yadashiv@sheridancollege.ca>
2019-10-07 08:30:03 +0530

    Added step 4

--- prime.py
+++ prime.py
@@ -4,10 +4,16 @@ prime.py -- Write the application code here
 def generate_prime_factors(number):
     """ Code to generate prime factors """
     prime_factor_list = []
+    i = 2
     if not isinstance(number, int):
         raise ValueError("Only integers can be used in the function")
 
     if number == 1:
         print(prime_factor_list)
+    elif (number%i) == 0:
+        prime_factor_list.append(2)
+    print(prime_factor_list)
 
     return number
+
+generate_prime_factors(2)
--- tests/test_generate_prime_factors.py
+++ tests/test_generate_prime_factors.py
@@ -7,7 +7,7 @@ from prime import generate_prime_factors
 def test_data_type_not_integer():
 
     """Step 1:Checks if the to be evaluated value is not an integer and raise a value error"""
-    if not isinstance(generate_prime_factors("crash"), int):
+    if not isinstance(generate_prime_factors(2), int):
         raise ValueError("Only integers can be used in the function")
 
 def test_empty_list_on_call_1():
@@ -22,4 +22,18 @@ def test_empty_list_on_call_2():
     prime_factor_list = []
     if (generate_prime_factors(2)%i) == 0:
         prime_factor_list.append(2)
+    print(prime_factor_list)
+
+def test_empty_list_on_call_3():
+    """Step 4: when function called for 3"""
+    i = 2
+    prime_factor_list = []
+    prime = generate_prime_factors(3) #assigned function to a variale
+    while i <= prime:
+        if (prime%i) == 0:
+            prime_factor_list.append(i)
+            prime = prime / i
+        else:
+            i = i+1
+
     print(prime_factor_list)
